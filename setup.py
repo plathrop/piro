9b411faaadebbbce9196ba2f3af2ed87b934f96a
Paul Lathrop <paul@simplegeo.com>
2011-05-19 09:52:43 -0700

    Initial project setup.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='piro',
+      version="0.0.1",
+      description='Piro is a tool for intelligently controlling services.',
+      author='Paul Lathrop',
+      author_email='paul@simplegeo.com',
+      url='https://github.com/plathrop/piro',
+      packages=find_packages(),
+      install_requires=[],
+      entry_points={'console_scripts':
+                        ['piro = piro.cli:main']}
+      )
